cc27cba2116ed530af3064ab737ec5f88ba37c2f
jitjangkul <chayanant.jitjangkul@unibw.de>
2024-03-26 02:07:57 +0100

    sign up

--- src/components/pages/SignUp.js
+++ src/components/pages/SignUp.js
@@ -1,6 +1,54 @@
 import React from 'react';
 import '../../App.css';
+import { Button } from '../Button';
+import CardItem from '../CardItem';
+import './SignUp.css';
+
+
 
 export default function SignUp() {
-  return <h1 className='sign-up'>SIGN UP HERE</h1>;
+  return (
+    <div className='sign-up'>
+      <div className='signup-container'>
+        <h1>LIMITED TIME OFFER</h1>
+        <p>Be the first to try our development kit</p>
+        <p>Sign up now and get a free use on your projects!</p>
+  
+        <div className='input-areas'>
+            <form>
+              <input
+                className='signup-name'
+                name='name'
+                type='text'
+                placeholder='Your Name'  
+              />
+              <br></br>
+              <input
+                className='signup-id'
+                name='email'
+                type='email'
+                placeholder='Email'
+              />
+              <br></br>
+              <input 
+                className='signup-org'
+                name='organization'
+                type='text'
+                placeholder='Organization'
+              />
+              <br></br>
+              <input 
+                className='signup-loc'
+                name='location'
+                type='text'
+                placeholder='Location'
+              />
+              <br></br>
+              <Button buttonStyle='btn--outline2'>Continue</Button>
+            </form>
+        </div>
+
+        </div>
+      </div>
+      );
 }
